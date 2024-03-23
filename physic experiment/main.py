d38f79fe914dfbbc88e4d16680d91a17ec6e9bbe
firefood <65716578+foodwithfire@users.noreply.github.com>
2024-03-23 14:23:28 +0100

    Update main.py

--- physic experiment/main.py
+++ physic experiment/main.py
@@ -1,10 +1,10 @@
-import pygame, sys, atom, random
+import pygame, sys, entity, random
 from settings import *
 
 screen = pygame.display.set_mode(screen_size)
 
-atoms = []
-AtomClass = atom.Atom
+entities = []
+EntityClass = entity.Entity
 
 clock = pygame.time.Clock()
 
@@ -12,15 +12,15 @@ while True:
     dt = clock.tick(60)
     screen.fill("white")
     if pygame.mouse.get_pressed()[0]:
-        atoms.append(AtomClass(pygame.math.Vector2(pygame.mouse.get_pos())))
+        entities.append(EntityClass(pygame.math.Vector2(pygame.mouse.get_pos())))
 
-    for atom in atoms:
-        atom.update(dt)
-        for other_atom in atoms:
-            if atom != other_atom:
-                atom.check_atom_collisions(other_atom.pos, dt, 2)
+    for entity in entities:
+        entity.update(dt)
+        for other_entity in entities:
+            if entity != other_entity:
+                entity.check_atom_collisions(other_entity.pos, dt, 2)
 
-        pygame.draw.circle(screen, atom.color, (atom.pos[0], atom.pos[1]), atom.size/2)
+        pygame.draw.circle(screen, entity.color, (entity.pos[0], entity.pos[1]), entity.size/2)
 
     pygame.display.flip()
     for event in pygame.event.get():
